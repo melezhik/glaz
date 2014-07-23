348dfcb2678c79c159616b031da6b7693cd9bf8d
Alexey Melezhik <melezhik@gmail.com>
2014-07-23 16:12:05 +0400

    reports-and-points

--- glaz/app/models/report.rb
+++ glaz/app/models/report.rb
@@ -1,2 +1,14 @@
 class Report < ActiveRecord::Base
+
+    belongs_to :host
+    belongs_to :report
+
+    def host
+        Host.find host_id
+    end
+
+    def report
+        Report.find report_id
+    end
+
 end
