1472118c727e3e9d0b0b91821addb3cd17b8d564
System User <glaz@moon5.adriver.x>
2014-08-20 13:47:04 +0400

    secret-key

--- glaz/config/initializers/devise.rb
+++ glaz/config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = 'dsjljlllllllljjjjdjjjjjjjjjjjjjjjjjjjjjjjjjj'
+  config.secret_key = ENV['SECRET_KEY_BASE']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
