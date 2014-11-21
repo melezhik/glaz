9f3d714a7fb3c4284cc419ad2a83b7bcf9d512e1
System User <glaz@glaz.adriver.x>
2014-11-21 17:11:30 +0300

    dj-logger

--- glaz/config/initializers/delayed_job.rb
+++ glaz/config/initializers/delayed_job.rb
@@ -1,4 +1,4 @@
 Delayed::Worker.destroy_failed_jobs = true
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'), 10, 1024*1000 )
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'), 1, 1024*1000 )
 Delayed::Worker.read_ahead = 1
 Delayed::Worker.sleep_delay = 0
